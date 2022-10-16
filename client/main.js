09015c37d6d4a09ffcaebd2dc583553f21b6b9ae
hanenAlrwashedaTus <alrwasheda@toechtersoehne.com>
2022-10-16 04:08:34 +0200

    adding content to card and some styles

--- client/main.js
+++ client/main.js
@@ -1,22 +1,68 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import data from './data.json'
 import './main.html';
+import './page.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+
+Template.page.onCreated(function pageOnCreated() {
+  this.questionIndex = new ReactiveVar(0);
+  this.question = new ReactiveVar(data[Template.instance().questionIndex.get()].question);
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.page.onRendered(function pageOnRendered() {
+  $('.prev').attr("disabled", "disabled");
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+Template.page.helpers({
+  question() {
+    return Template.instance().question.get();
   },
+  questionIndex() {
+    return Template.instance().questionIndex.get();
+  },
+  currentQuestion() {
+    return Template.instance().questionIndex.get() + 1;
+  },
+  totalQuestionNumber() {
+    return data.length;
+  },
+  currentQuestionOptions() {
+    return data[Template.instance().questionIndex.get()].options;
+  }
+});
+Template.page.events({
+  'click .prev'(event, instance) {
+    const questionIndex = instance.questionIndex;
+    if (questionIndex.get() !== 0) {
+      questionIndex.set(questionIndex.get() - 1);
+      instance.question.set(data[questionIndex.get()].question);
+    }
+    if (questionIndex.get() === 0) {
+      $('.prev').attr("disabled", "disabled");
+    }
+    if (questionIndex.get() === data.length - 2) {
+      $('.next').removeAttr("disabled");
+    }
+  },
+  'click .next'(event, instance) {
+    const questionIndex = instance.questionIndex;
+    if (questionIndex.get() !== data.length - 1) {
+      questionIndex.set(questionIndex.get() + 1);
+      instance.question.set(data[questionIndex.get()].question);
+    }
+    if (questionIndex.get() === 1) {
+      $('.prev').removeAttr("disabled");
+    }
+    if (questionIndex.get() === data.length - 1) {
+      $('.next').attr("disabled", "disabled");
+    }
+  },
+  'click .option'(event, instance) {
+    //check if any item has already been chosen
+    $('.options').children().toArray().forEach(op => {
+      op.classList.remove('selected')
+    });
+    $(event.target).addClass('selected') //set color to selected item
+  }
 });
